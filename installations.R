785ab9d41a79e20c360952a3dfee97658b965e64
William Figgett <wfiggett@gmail.com>
2024-11-01 13:21:37 +1100

    Add downloading steps for reference files
    Bulky reference files can be downloaded once from sourcing the installations.R script.

--- installations.R
+++ installations.R
@@ -11,3 +11,40 @@ packages <- c("shiny", "Cairo", "shinycssloaders", "shinybusy",
 BiocManager::install(packages)
 remotes::install_github("ricardo-bion/ggradar")
 renv::snapshot()
+
+
+# download public resources ----
+
+## STRING DB ----
+
+STRING_URL <- "https://stringdb-downloads.org/download/protein.links.v12.0/9606.protein.links.v12.0.txt.gz"
+temp <- tempfile()
+download.file(STRING_URL,temp)
+unz(temp, "Data/STRING/9606.protein.links.v12.0.txt")
+unlink(temp)
+
+## Gene Ontology (GO) ----
+GOA_URL <- "https://current.geneontology.org/annotations/goa_human.gaf.gz"
+GO_BASIC_OBO_URL <- "http://current.geneontology.org/ontology/go-basic.obo"
+temp <- tempfile()
+download.file(GOA_URL,temp)
+unz(temp, "Data/GO/goa_human.gaf")
+unlink(temp)
+temp <- tempfile()
+download.file(GO_BASIC_OBO_URL,temp)
+unlink(temp)
+
+## HPO ----
+#"Data/HPO/HPO_genes.tsv"
+
+## OMIM ----
+# Data/OMIM/mim2geneDedup.tsv"
+
+## ClinVar ----
+# CLINVAR_URL <- ""
+# temp <- tempfile()
+# download.file(CLINVAR_URL,temp)
+# unz(temp, "Data/ClinVar/goa_human.gaf")
+# unlink(temp)
+
+
